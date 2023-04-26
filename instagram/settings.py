2959be6d6961224c3145da2563240504decfb32c
bmj0053 <bmj05378@gmail.com>
2023-04-26 21:12:19 +0900

    Modify settings.py

--- instagram/settings.py
+++ instagram/settings.py
@@ -10,17 +10,32 @@ For the full list of settings and their values, see
 https://docs.djangoproject.com/en/3.2/ref/settings/
 """
 
+import json, os
+from django.core.exceptions import ImproperlyConfigured
 from pathlib import Path
+
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
 
+secret_file = os.path.join(BASE_DIR, 'secrets.json')    # secrets.json 파일에 암호키 저장
+
+with open(secret_file) as f:
+    secrets = json.loads(f.read())
+
+def get_secret(setting):
+    try:
+        return secrets[setting]
+    except KeyError:
+        error_msg = "Set the {} environment variable".format(setting)
+        raise ImproperlyConfigured(error_msg)
+
+SECRET_KEY = get_secret("SECRET_KEY")
 
 # Quick-start development settings - unsuitable for production
 # See https://docs.djangoproject.com/en/3.2/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-_(1*xa-atj-du+y3utq-np&xf$aa#yoxwl&w&$&@#s8-^i@ojb'
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
