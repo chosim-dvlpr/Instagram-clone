b88c7f1f8b470ac279edff567055d2b2a06a687a
bmj0053 <bmj05378@gmail.com>
2023-05-09 22:17:30 +0900

    Modify settings.py : Add MEDIA_URL & ROOT

--- instagram/settings.py
+++ instagram/settings.py
@@ -139,3 +139,7 @@ STATIC_URL = '/static/'
 # https://docs.djangoproject.com/en/3.2/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
+
+# 사용자가 올리는 이미지 관리
+MEDIA_URL = '/media/'
+MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
